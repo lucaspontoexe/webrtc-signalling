3d171f1190b6cd388a0aa3b3589572676c366e97
lucaspontoexe <ltx16@hotmail.com>
2021-03-12 13:55:59 -0300

    add event handling

--- client/client.js
+++ client/client.js
@@ -1,12 +1,14 @@
-import Peer from 'https://cdn.skypack.dev/simple-peer';
+import Peer from "https://cdn.skypack.dev/simple-peer";
 
-// /**
-//  * @typedef IncomingSignalMessage
-//  * @property 
-//  */
+/**
+ * @typedef IncomingSignalMessage
+ * @property {string} type
+ * @property {string} origin
+ * @property {string} signal
+ */
 
+// TODO: nickname -> id
 
-const myself = new Peer({initiator: true});
 const peersByID = new Map();
 const peers = [];
 /**
@@ -15,52 +17,71 @@ const peers = [];
 let websocket;
 
 function init() {
-    websocket = new WebSocket('ws://localhost:9999/?nickname="peganobreu"');
-    websocket.onmessage = handleWebSocketMessage;
+  websocket = new WebSocket('ws://localhost:9999/?nickname="peganobreu"');
+  websocket.onmessage = handleWebSocketMessage;
 }
 
 /**
  * Handle Message
- * @param {MessageEvent<string>} event 
+ * @param {MessageEvent<string>} event
  */
 function handleWebSocketMessage(event) {
-    const message = JSON.parse(event.data);
-    // todo: handle signal messages
+  const message = JSON.parse(event.data);
+  // todo: handle signal messages
+  switch (message.type) {
+    case "welcome":
+      onWelcome(message.peers);
+      break;
+
+    case "signal":
+      onSignalReceived(message.origin, message.signal);
+      break;
+
+    default:
+      break;
+  }
 }
 
-function onWelcome() {
-    for (const peer of peers) {
-        addPeer(peer);
-    }
+// EVENTS (?)
+function onWelcome(peers) {
+  for (const peer of peers) {
+    addPeer(peer);
+  }
 }
 
+// SIGNALLING
+function onSignalReceived(origin, signal) {
+  peersByID.get(origin).signal(signal);
+}
 
+function sendSignal(recipient, signal) {
+  // opcional: formatar como objeto, pra ver de onde tá vindo (ou não)
+  // temos type?
+  websocket.send(JSON.stringify({ signal, recipient }));
+}
+
+// ADD & REMOVE PEER
 function addPeer(nickname) {
-    const p = new Peer();
-        p.on('signal', data => sendSignal(data, nickname));
-        p.on('data', data => handlePeerMessage(nickname, JSON.parse(data)));
-        p.on('close', () => removePeer(nickname));
-        peersByID.set(nickname,p);
-        peers.push({nickname, peer: p});
+  const p = new Peer({ initiator: true });
+  p.on("signal", (data) => sendSignal(data, nickname));
+  p.on("data", (data) => handlePeerMessage(nickname, JSON.parse(data)));
+  p.on("close", () => removePeer(nickname));
+  peersByID.set(nickname, p);
+  peers.push({ nickname, peer: p });
 }
 
 function removePeer(nickname) {
-    peersByID.delete(nickname);
-    peers = peers.filter(item => item.nickname !== nickname);
+  peersByID.delete(nickname);
+  peers = peers.filter((item) => item.nickname !== nickname);
 }
 
-function sendSignal(signal, recipient) {
-    // opcional: formatar como objeto, pra ver de onde tá vindo (ou não)
-    // temos type?
-    websocket.send(JSON.stringify({signal, recipient}));
-}
-
+// WEBRTC MESSAGE CONTROL
 function handlePeerMessage(nickname, message) {
-    console.log(nickname, 'sent: ', message);
+  console.log(nickname, "sent: ", message);
 }
 
 function broadcastMessage(message) {
-    for (const peer of peers) {
-        peer.send(JSON.parse(message));
-    }
+  for (const peer of peers) {
+    peer.send(JSON.parse(message));
+  }
 }
