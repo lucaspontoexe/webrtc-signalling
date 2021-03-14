7788f9a27ed286c10dc8725771c5d5431f6331da
lucaspontoexe <ltx16@hotmail.com>
2021-03-13 23:58:30 -0300

    temos jsdoc

--- server/client/client.js
+++ server/client/client.js
@@ -1,22 +1,57 @@
+// @ts-check
+// @ts-ignore
 import Peer from "https://cdn.skypack.dev/simple-peer-light";
 
 /**
  * @typedef IncomingSignalMessage
- * @property {string} type
+ * @property {"signal"} type
  * @property {string} origin
  * @property {string} signal
  */
 
-const peersByID = new Map();
-let peers = [];
-let myID = "peganobreu";
 /**
- * @type WebSocket
+ * @typedef WelcomeMessage
+ * @property {"welcome"} type
+ * @property {string[]} peers
  */
 
+/**
+ * @typedef PeerJoinedMessage
+ * @property {"peer_joined"} type
+ * @property {string} id
+ */
+
+/**
+ * @typedef PeerLeftMessage
+ * @property {"peer_left"} type
+ * @property {string} id
+ */
+
+/**
+ * @typedef PeerList
+ * @property {string} id
+ * @property {import('./SimplePeer').Instance} peer
+ */
+
+/**
+ * @type {Map<string, import('./SimplePeer').Instance>}
+ */
+const peersByID = new Map();
+
+/**
+ * @type {Array<PeerList>}
+ */
+let peers = [];
+let myID = "peganobreu";
+
+/**
+ * @type {WebSocket}
+ */
+let websocket;
+
 function init() {
-  window.websocket = new WebSocket(`ws://${window.location.host}/?id=${myID}`);
-  window.websocket.onmessage = handleWebSocketMessage;
+  websocket = new WebSocket(`ws://${window.location.host}/?id=${myID}`);
+  websocket.onmessage = handleWebSocketMessage;
 }
 
 /**
@@ -24,6 +59,9 @@ function init() {
  * @param {MessageEvent<string>} event
  */
 function handleWebSocketMessage(event) {
+  /**
+   * @type {IncomingSignalMessage | WelcomeMessage | PeerJoinedMessage | PeerLeftMessage}
+   */
   const message = JSON.parse(event.data);
   console.log(message);
   switch (message.type) {
@@ -52,17 +90,28 @@ function handleWebSocketMessage(event) {
 }
 
 // EVENTS (?)
+/**
+ * @param {string[]} peers
+ */
 function onWelcome(peers) {
   console.log(peers);
   peers.forEach((peer) => addPeer(peer, { initiator: true }));
 }
 
 // SIGNALLING
+/**
+ * @param {string} origin
+ * @param {object} signal
+ */
 function onSignalReceived(origin, signal) {
   console.log("received signal", signal);
   peersByID.get(origin).signal(JSON.stringify(signal));
 }
 
+/**
+ * @param {string} recipient
+ * @param {object} signal
+ */
 function sendSignal(recipient, signal) {
   // opcional: formatar como objeto, pra ver de onde tá vindo (ou não)
   // temos type?
@@ -72,18 +121,28 @@ function sendSignal(recipient, signal) {
 }
 
 // ADD & REMOVE PEER
+/**
+ * @param {string} id
+ * @param {{ initiator: boolean; }} config
+ */
 function addPeer(id, config) {
   if (id === myID) return;
 
+  /**
+   * @type {import('./SimplePeer').Instance}
+   */
   const p = new Peer(config);
-  p.on("signal", (data) => sendSignal(id, data));
-  p.on("data", (data) => handlePeerMessage(id, JSON.parse(data)));
+  p.on("signal", (/** @type {object} */ data) => sendSignal(id, data));
+  p.on("data", (/** @type {string} */ data) => handlePeerMessage(id, JSON.parse(data)));
   p.on("close", () => removePeer(id));
   p.on("error", console.log);
   peersByID.set(id, p);
   peers.push({ id, peer: p });
 }
 
+/**
+ * @param {string} id
+ */
 function removePeer(id) {
   addToLog(`${id} saiu`);
   peersByID.delete(id);
@@ -91,10 +150,17 @@ function removePeer(id) {
 }
 
 // WEBRTC MESSAGE CONTROL
+/**
+ * @param {string} id
+ * @param {{ message: any; }} message
+ */
 function handlePeerMessage(id, message) {
   addToLog(`${id}: ${message.message}`);
 }
 
+/**
+ * @param {{ message: any; }} message
+ */
 function broadcastMessage(message) {
   // precisa disso?
   addToLog(`${myID}: ${message.message}`);
@@ -105,22 +171,24 @@ function broadcastMessage(message) {
 
 // HTML AND INTERACTION CODE
 /**
- * @type HTMLButtonElement
+ * @type {HTMLButtonElement}
  */
 const connectButton = document.querySelector("#connectButton");
 /**
- * @type HTMLButtonElement
+ * @type {HTMLButtonElement}
  */
 const sendButton = document.querySelector("#sendButton");
 /**
- * @type HTMLInputElement
+ * @type {HTMLInputElement}
  */
 const idInput = document.querySelector("#idInput");
 /**
- * @type HTMLInputElement
+ * @type {HTMLInputElement}
  */
 const messageInput = document.querySelector("#messageInput");
-
+/**
+ * @type {HTMLPreElement}
+ */
 const outputElement = document.querySelector(".output");
 
 document.querySelector("form").onsubmit = (event) => {
@@ -135,6 +203,9 @@ connectButton.onclick = () => {
 
 sendButton.onclick = () => broadcastMessage({ message: messageInput.value });
 
+/**
+ * @param {string} message
+ */
 function addToLog(message) {
   outputElement.innerText += message + "\n";
 }
